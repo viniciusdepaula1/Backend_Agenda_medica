072fc475357a2a5f98c358de176fed0a8b6ee568
Vinicius_de_Paula <viniciusdepaula199@gmail.com>
2021-03-27 16:25:22 -0300

    fix: all routes with middleware

--- src/routes/User.js
+++ src/routes/User.js
@@ -6,7 +6,7 @@ const auth = require('./middlewares/auth');
 // cada controller define as funções que serão executadas em cada endpoint. 
 const UserController = require('../controllers/User');
 
-router.get('/list', UserController.list);
+router.get('/list', auth.checkIfAuthenticated, UserController.list);
 router.post('/create', auth.checkIfAuthenticated, UserController.create);
 router.delete('/delete', auth.checkIfAuthenticated, UserController.delete);
 
